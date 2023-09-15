141044bc430215083dd7ff703862c91c0933b2d3
Raefandy Fadila <RaefandyF@MacBook-Pro.local>
2023-09-16 01:30:00 +0700

    menambahkan halaman login

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,32 +22,41 @@ function Home() {
               </h3>
             </div>
             <div className="flex justify-center items-center mt-5">
-              <img src={buttonGif} alt="gif1" className="w-20 h-20 py-0" />
+              <img src={buttonGif} alt="gif1" className="w-20 h-20 py-0"/>
             </div>
           </div>
         </div>
       </div>
-      <div className="bg position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-        <div
-          className="mainPage col-md-5 p-lg-5 mx-auto my-5"
-          style={{ width: "70rem" }}
-        >
-          <h1 className="mainText display-4 fw-normal">
-            Senang Bertemu Anda Kembali
-          </h1>
-          <p style={{ color: "white" }} className="mainText lead fw-normal">
-            Jangan Menyerah Lanjutkan
-          </p>
-          <input
-            className="mainSearch"
-            style={{ borderRadius: "0.5rem" }}
-            type="text"
-            placeholder="Search"
-          />
+
+      <div className="home-page2 position-relative overflow-hidden bg-light">
+        <div className="mainPage col-md-5 p-lg-5 mx-auto mt-32" style={{width:"70rem"}}>
+            <h1 className="mainText display-4 fw-normal text-white text-center">
+              Senang Bertemu Anda Kembali
+            </h1>
+            <p className="mainText lead fw-normal text-white text-center text-2xl">
+              Jangan Menyerah! Lanjutkan capaian diet anda!
+            </p>
+            <p className="text-white text-center text-2xl">
+            Login Untuk Melanjutkan
+            </p>
+            <div className="w-3/5 ml-52 bg-lime-800 mt-3 rounded-md">
+              <form>
+                <div className="form-group w-4/5 ml-28 mt-4">
+                  <label className="mt-4 text-white text-2xl" for="exampleInputEmail1">Username</label>
+                  <br></br>
+                  <input type="email" className="form-control w-5/6" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                </div>
+                <div className="form-group w-4/5 ml-28 mt-3">
+                  <label className="text-white text-2xl" for="exampleInputPassword1">Password</label>
+                  <input type="password" className="form-control w-5/6" id="exampleInputPassword1"></input>
+                </div>
+                <button type="submit" style={{marginLeft: "45%"}} className="btn bg-white my-4 text-lime-800 text-2xl">
+                Login
+                </button>
+              </form>
+            </div>
         </div>
-        <img src={require("../Assets/login-bg.JPG")}></img>
       </div>
-      <div className="product-device product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
   );
 }
