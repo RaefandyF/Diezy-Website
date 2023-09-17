8e2b584154b91ea3f209d6727610135eeb8290f0
Raefandy Fadila <RaefandyF@MacBook-Pro.local>
2023-09-17 23:01:34 +0700

    Menambahkan halaman 2 home

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,8 @@ import buttonGif from "../Assets/button.gif";
 
 function Home() {
   return (
+
+    // Halaman 1
     <div className="no-scrollbar">
       <div className="home-page1 py-5">
         <div className="h-3/4 flex justify-center items-center">
@@ -28,7 +30,47 @@ function Home() {
         </div>
       </div>
 
+      {/* Halaman 2 */}
       <div className="home-page2 position-relative overflow-hidden bg-light">
+        <div className="mainPage col-md-5 p-lg-5 mx-auto mt-32" style={{width:"70rem"}}>
+            <h1 className="mainText display-4 fw-normal text-white text-center">
+              Izinkan Kami Mengenali Anda
+            </h1>
+            <div className="w-9/12 ml-32 bg-lime-800 mt-3 rounded-md">
+              <form>
+                <div className="form-group w-5/5 ml-10 mt-4">
+                  <label className="mt-4 text-white text-2xl" for="inputRegisterName">Nama</label>
+                  <br></br>
+                  <input type="text" className="form-control w-11/12" id="inputRegisterNama"></input>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-6">
+                      <div className="form-group w-4/5 ml-7 mt-4">
+                        <label className="text-white text-2xl" for="inputRegisterDate">Tanggal lahir</label>
+                        <input type="date" className="form-control" id="inputRegisterDate"></input>
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div className="form-group ml-7 w-4/5">
+                        <label className="mt-4 text-white text-2xl" for="inputRegisterGender">Jenis Kelamin</label>
+                        <br></br>
+                        <input type="text" className="form-control" id="inputRegisterGender"></input>
+                      </div>
+                    </div>
+                </div>  
+                </div>
+                <button type="submit" style={{marginLeft: "44%"}} className="btn bg-white mt-9 mt-20 text-lime-800 text-2xl">
+                Lanjut
+                </button>
+              </form>
+              <p className="text-white">Sudah Pernah Mencoba Diezy? <u style={{cursor:"pointer"}}>Login</u></p>
+            </div>
+        </div>
+      </div>
+
+    {/* Halaman 3 */}
+      <div className="home-page3 position-relative overflow-hidden bg-light">
         <div className="mainPage col-md-5 p-lg-5 mx-auto mt-32" style={{width:"70rem"}}>
             <h1 className="mainText display-4 fw-normal text-white text-center">
               Senang Bertemu Anda Kembali
@@ -37,14 +79,14 @@ function Home() {
               Jangan Menyerah! Lanjutkan capaian diet anda!
             </p>
             <p className="text-white text-center text-2xl">
-            Login Untuk Melanjutkan
+              Login Untuk Melanjutkan
             </p>
             <div className="w-3/5 ml-52 bg-lime-800 mt-3 rounded-md">
               <form>
                 <div className="form-group w-4/5 ml-28 mt-4">
-                  <label className="mt-4 text-white text-2xl" for="exampleInputEmail1">Username</label>
+                  <label className="mt-4 text-white text-2xl" for="inputUsername">Username</label>
                   <br></br>
-                  <input type="email" className="form-control w-5/6" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                  <input type="text" className="form-control w-5/6" id="inputUsername" aria-describedby="emailHelp"></input>
                 </div>
                 <div className="form-group w-4/5 ml-28 mt-3">
                   <label className="text-white text-2xl" for="exampleInputPassword1">Password</label>
